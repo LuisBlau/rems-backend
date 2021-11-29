6eca4793ff11d428039b5a8cae25e85de27765b5
Dallas White <dallas.white@toshibagcs.com>
2021-11-29 11:25:41 -0600

    start on extract table

--- routes/registers.js
+++ routes/registers.js
@@ -1,4 +1,11 @@
 const sqlString = require('sqlstring')
+const atob = require("atob")
+const btoa = require("btoa")
+
+const { ServiceBusClient } = require("@azure/service-bus");
+const sbClient = new ServiceBusClient("Endpoint=sb://remscomm.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Dk+TDFecPYBkRKtCqqudv1dnrN2hR5bcEN1t1alztOI=");
+var azureClient = new require("mongodb").MongoClient("mongodb://pas-test-nosql-db:1Xur1znUvMn4Ny2xW4BwMjN1eHXYPpCniT8eU3nfnnGVtbV7RVUDotMz9E7Un226yrCyjXyukDDSSxLjNUUyaQ%3D%3D@pas-test-nosql-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@pas-test-nosql-db@");
+azureClient.connect();
 
 function formatCount(resp) {
   // return Object.assign({}, resp.map((x) => ({
@@ -111,5 +118,69 @@ module.exports = function (app, connection, log) {
         }
       })
   })
+  app.get('/registers/extracts', (req, res) => {
+/* 	var results = [{ 
+   "Retailer": "T0BGBBL",
+   "RegNum": "44",
+   "Store": "US0303",
+   "Timestamp": "2021/11/11 16:00:00",
+   "values": {
+     "File":"SCS_Extract_Lane_S303_L044_RMA Default Policy_20211028_164049_CST.zip",
+      "InstalledPath":"/cdrive/ext/signed/chec/IBMSelfCheckout",
+      "ExtractType":"Extract",
+	  "State":"Ohio"
+   },
+   "location": {
+      "Store":"true",
+      "Azure":"false",
+      "AzureUrl":""
+   }
+},
+{ 
+   "Retailer": "T0BGBBL",
+   "RegNum": "45",
+   "Store": "US0303",
+   "Timestamp": "2021/11/11 16:00:00",
+   "values": {
+     "File":"SCS_Extract_Lane_S303_L044_RMA Default Policy_20211028_164049_CST.zip",
+      "InstalledPath":"/cdrive/ext/signed/chec/IBMSelfCheckout",
+      "ExtractType":"Extract",
+	  "State":"Ohio"
+   },
+   "location": {
+      "Store":"true",
+      "Azure":"false",
+      "AzureUrl":""
+   }
+}
+] */
+   var results = []
+   var snapshots = azureClient.db("pas_reloads").collection("extracts");
+   snapshots.find().toArray(function(err, result){
+     results = result;
+     console.log(result)
+	let modifiedResults = []
+	for (var x of results) {
+		var y = x
+	    y["InStore"] = x["location"]["Store"]
+		y["SBreqLink"] = "http://127.0.0.1:3001/registers/extracts/" + btoa(unescape(encodeURIComponent(JSON.stringify(x).replace("/\s\g",""))))
+		y["ExtractType"] = x["values"]["ExtractType"]
+		y["State"] = x["values"]["State"]
+		modifiedResults.push(y)
+	}
+	res.send(modifiedResults)
+  });
+  });
+  
 
+  app.get('/registers/extracts/:string', (req, res) => {
+	  j = JSON.parse(atob(req.params["string"]))
+	  msgSent = {
+		  "retailer":j.retailer,
+		  "store":j.store, 
+		  "filename":j.values.file
+	  };
+    const sender = sbClient.createSender("storefilerequestsubscription");
+    sender.sendMessages(msgSent);
+  })
 }
