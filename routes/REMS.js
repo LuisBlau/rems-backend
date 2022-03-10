db504be2fc36b28a5c40dd716c3a3fff599fead1
Gordon Neeley <Gordon.Neeley@toshibagcs.com>
2022-03-09 20:04:09 -0600

    TK-113614: Changed error handling.
    TK-113614: Updates to handle deployment schedule page.

--- routes/REMS.js
+++ routes/REMS.js
@@ -1,10 +1,16 @@
 // Std Library
-const {readFileSync} = require('fs')
+const { readFileSync } = require('fs')
 const path = require('path')
 const multiparty = require('multiparty');
 const fs = require('fs');
 const readline = require('readline');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const _ = require('lodash');
+const { isRegExp } = require('lodash');
+const statusCode = require('http-status-codes').StatusCodes
+
+
+
 // setup dirs
 var uploadDir = process.env.REMS_HOME + "/uploads";
 
@@ -19,151 +25,235 @@ readRetailerId();
 
 
 function readRetailerId() {
-  const fileStream = fs.createReadStream(process.env.REMS_HOME +"/etc/com.toshibacommerce.service.cloudforwarder.cfg");
+    const fileStream = fs.createReadStream(process.env.REMS_HOME + "/etc/com.toshibacommerce.service.cloudforwarder.cfg");
 
-  const lineReader = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
+    const lineReader = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
 
-  lineReader.on('line', function (line) {
-    if ( line.includes("retailer-torico-id") )
-    {
-        var values = line.split("=");
-        retailerId = values[1];
-    }
-  });
+    lineReader.on('line', function (line) {
+        if (line.includes("retailer-torico-id")) {
+            var values = line.split("=");
+            retailerId = values[1];
+        }
+    });
 
 }
 
 function sendRelevantJSON(res, jsonPath) {
-  res.send(JSON.parse(
-    readFileSync(
-      path.join(process.cwd(), 'Data', jsonPath)
-    )
-  ))
+    res.send(JSON.parse(
+        readFileSync(
+            path.join(process.cwd(), 'Data', jsonPath)
+        )
+    ))
 }
 
 module.exports = function (app, connection, log) {
 
 
-  app.get('/REMS/store-connection', (req, res) => {
-    log.info(`GET ${req.originalUrl}`)
+    app.get('/REMS/store-connection', (req, res) => {
+        log.info(`GET ${req.originalUrl}`)
 
-    sendRelevantJSON(res, 'store_connection.json');
+        sendRelevantJSON(res, 'store_connection.json');
 
-  })
+    })
 
-  app.get('/REMS/vpd', (req, res) => {
-    log.info(`GET ${req.originalUrl}`)
+    app.get('/REMS/vpd', (req, res) => {
+        log.info(`GET ${req.originalUrl}`)
 
-    sendRelevantJSON(res, 'out_vpd_filtered.json');
-  })
+        sendRelevantJSON(res, 'out_vpd_filtered.json');
+    })
 
-  app.get('/REMS/release', (req, res) => {
-    log.info(`GET ${req.originalUrl}`)
+    app.get('/REMS/release', (req, res) => {
+        log.info(`GET ${req.originalUrl}`)
 
-    sendRelevantJSON(res, 'out_release.json');
-  })
+        sendRelevantJSON(res, 'out_release.json');
+    })
 
-  app.get('/REMS/low-mem', (req, res) => {
-    log.info(`GET ${req.originalUrl}`)
+    app.get('/REMS/low-mem', (req, res) => {
+        log.info(`GET ${req.originalUrl}`)
 
-    sendRelevantJSON(res, 'low_mem.json');
-  })
-  app.post("/REMS/uploadfile", (req,res) => {
-    console.log("request recieved")
-    var form = new multiparty.Form();
-    var filename;
-    form.parse(req, function(err, fields, files) {
-	    res.writeHead(200, { 'content-type': 'text/plain' });
-      res.write('received upload:\n\n');
-      if (!fs.existsSync(uploadDir)){
-        fs.mkdirSync(uploadDir);
-      }
-	  let newFileName = uploadDir + "/" + files["file"][0].originalFilename
-    filename = files["file"][0].originalFilename;
-	  if(fs.existsSync(newFileName)) {
-	    newFileName = uploadDir + "/" + files["file"][0].originalFilename + Math.floor(+new Date() / 1000).toString()
-	  }
-	  fs.copyFile(files["file"][0].path, newFileName, (err) => {
-        if (err) throw err;
-      });
-    });
+        sendRelevantJSON(res, 'low_mem.json');
+    })
+    app.post("/REMS/uploadfile", (req, res) => {
+        console.log("request recieved")
+        var form = new multiparty.Form();
+        var filename;
+        form.parse(req, function (err, fields, files) {
+            res.writeHead(200, { 'content-type': 'text/plain' });
+            res.write('received upload:\n\n');
+            if (!fs.existsSync(uploadDir)) {
+                fs.mkdirSync(uploadDir);
+            }
+            let newFileName = uploadDir + "/" + files["file"][0].originalFilename
+            filename = files["file"][0].originalFilename;
+            if (fs.existsSync(newFileName)) {
+                newFileName = uploadDir + "/" + files["file"][0].originalFilename + Math.floor(+new Date() / 1000).toString()
+            }
+            fs.copyFile(files["file"][0].path, newFileName, (err) => {
+                if (err) throw err;
+            });
+        });
 
-    //query biggest index
-    var uploads = azureClient.db("pas_software_distribution").collection("uploads");
-    var results = [];
-    uploads.find({retailer_id:retailerId}).sort({id:-1}).limit(1).toArray(function(err, result){
-      results = result;
-      var index = results[0].id;
-      index++;
-    
-      var currentdate = new Date(); 
-      var datetime = currentdate.getFullYear() + "-"
-                + ((currentdate.getMonth()+1 < 10)?"0":"")+(currentdate.getMonth()+1) + "-"  
-                + ((currentdate.getDate() < 10)?"0":"")+currentdate.getDate() + " "  
-                + ((currentdate.getHours() < 10)?"0":"")+currentdate.getHours() + ":"  
-                + ((currentdate.getMinutes() < 10)?"0":"")+currentdate.getMinutes() + ":"  
-                + ((currentdate.getSeconds() < 10)?"0":"")+currentdate.getSeconds();  
+        //query biggest index
+        var uploads = azureClient.db("pas_software_distribution").collection("uploads");
+        var results = [];
+        uploads.find({ retailer_id: retailerId }).sort({ id: -1 }).limit(1).toArray(function (err, result) {
+            results = result;
+            var index = results[0].id;
+            index++;
 
-      var newFile = {id:index.toString(),retailer_id: retailerId, filename:filename, inserted:currentdate.getTime(),timestamp:datetime,archived:"false"};
-    
-      uploads.insertOne(newFile, function(err, res) {
-        if (err) throw err;
-      });
+            var currentdate = new Date();
+            var datetime = currentdate.getFullYear() + "-"
+                + ((currentdate.getMonth() + 1 < 10) ? "0" : "") + (currentdate.getMonth() + 1) + "-"
+                + ((currentdate.getDate() < 10) ? "0" : "") + currentdate.getDate() + " "
+                + ((currentdate.getHours() < 10) ? "0" : "") + currentdate.getHours() + ":"
+                + ((currentdate.getMinutes() < 10) ? "0" : "") + currentdate.getMinutes() + ":"
+                + ((currentdate.getSeconds() < 10) ? "0" : "") + currentdate.getSeconds();
 
-    });
+            var newFile = { id: index.toString(), retailer_id: retailerId, filename: filename, inserted: currentdate.getTime(), timestamp: datetime, archived: "false" };
 
-  });
-  
-  app.get('/REMS/uploads', (req, res) => {
-    var results = []
-    var uploads = azureClient.db("pas_software_distribution").collection("uploads");
-    uploads.find( {retailer_id:retailerId}).toArray(function(err, result){
-      results = result;
-      console.log(result)
-  
-    res.send(results)
-    });
-  });
-  
-  app.post('/sendCommand',bodyParser.json(), (req, res) => {
-    res.send('received upload:\n\n');
-    console.log("New command set");
-    console.log(req.body)
+            uploads.insertOne(newFile, function (err, res) {
+                if (err) throw err;
+            });
 
-    //query biggest index
-    var deployConfig = azureClient.db("pas_software_distribution").collection("deploy-config");
-    var results = [];
-    deployConfig.find({retailer_id:retailerId}).sort({id:-1}).limit(1).toArray(function(err, result){
-      results = result;
-      var index = results[0].id;
-      index++;
-    
-      var toInsert = {
-        id:index.toString(),
-        name:"Missing name",
-        retailer_id:retailerId,
-        config_steps:req.body
-      }
-      
-      deployConfig.insertOne(toInsert, function(err, res) {
-        if (err) throw err;
-      });
+        });
 
     });
-  })
 
-  app.get('/REMS/deploys', (req, res) => {
-    var results = []
-    var deploys = azureClient.db("pas_software_distribution").collection("deployments");
-    deploys.find( {retailer_id:retailerId, status:{$ne:"Succeeded"}}).toArray(function(err, result){
-      results = result;
-      console.log(result)
-  
-    res.send(results)
+    app.get('/REMS/uploads', (req, res) => {
+        var results = []
+        var uploads = azureClient.db("pas_software_distribution").collection("uploads");
+        uploads.find({ retailer_id: retailerId }).toArray(function (err, result) {
+            results = result;
+            console.log(result)
+
+            res.send(results)
+        });
     });
-  });
+
+    app.post('/sendCommand', bodyParser.json(), (req, res) => {
+        res.send('received upload:\n\n');
+        console.log("New command set");
+        console.log(req.body)
+
+        //query biggest index
+        var deployConfig = azureClient.db("pas_software_distribution").collection("deploy-config");
+        var results = [];
+        deployConfig.find({ retailer_id: retailerId }).sort({ id: -1 }).limit(1).toArray(function (err, result) {
+            results = result;
+            var index = results[0].id;
+            index++;
+
+            var toInsert = {
+                id: index.toString(),
+                name: "Missing name",
+                retailer_id: retailerId,
+                steps: req.body
+            }
+
+            deployConfig.insertOne(toInsert, function (err, res) {
+                if (err) throw err;
+            });
+
+        });
+    })
+
+    app.get('/REMS/deploys', (req, res) => {
+        var results = []
+        var deploys = azureClient.db("pas_software_distribution").collection("deployments");
+        deploys.find({ retailer_id: retailerId, status: { $ne: "Succeeded" } }).toArray(function (err, result) {
+            results = result;
+            console.log(result)
+
+            res.send(results)
+        });
+    });
+
+    app.get('/REMS/deploy-configs', (req, res) => {
+        console.log("GET deploy-configs request ")
+        var results = [];
+        const configs = azureClient.db("pas_software_distribution").collection("deploy-config");
+        configs.find({ retailer_id: retailerId, name: { $ne: "Missing name" } }, {
+            projection: { steps: false, retailer_id: false, _id: false }
+        }).toArray(function (err, result) {
+            results = result;
+            res.send(results);
+        });
+    });
+
+    app.post('/deploy-config', bodyParser.json(), (req, res) => {
+        console.log("POST deploy-config recived", req.body)
+
+        const dateTime = req.body.dateTime;
+        const name = req.body.name
+        const id = req.body.id
+
+        const configs = azureClient.db("pas_software_distribution").collection("deploy-config");
+        configs.findOne({ name: name, id: id }, function (err, config) {
+
+            if (err) {
+                const msg = {"error": err}
+                console.log(msg)
+                res.status(statusCode.INTERNAL_SERVER_ERROR).json(msg)
+                throw err
+            } else if (!config) {
+                const msg = {"message":"Deploy-Config: name and id does not exist"}
+                console.log(msg)
+                res.status(statusCode.NO_CONTENT).json(msg);
+            }
+            else {
+                console.log("config found : \n\n"/*, config*/)
+
+                var record = {};
+                record.id = 0
+                record.retailer_id = config.retailer_id;
+                record.apply_time = dateTime;
+                record.storeName = "";
+                record.agentName = "";
+                record.status = "initial";
+                record.steps = config.steps;
+
+                for (const i in record.steps) {
+                    record.steps[i].status = 'initial'
+                    record.steps[i].output = []
+                }
+
+                const deployments = azureClient.db("pas_software_distribution").collection("deployments");
+                deployments.find({}).sort({ id: -1 }).limit(1).toArray(function (err, maxResults) {
+
+                    if (err) {
+                        const msg = {"error": err}
+                        console.log(msg)
+                        res.status(statusCode.INTERNAL_SERVER_ERROR).json(msg)
+                        throw err
+                    }
+
+                    var maxId = maxResults[0].id;
+                    var newRecords = [];
+                    req.body.storeList.split(',').forEach(val => {
+                        const info = val.split(':');
+                        record.storeName = info[0].trim();
+                        record.agentName = info[1].trim();
+                        record.id = ++maxId;
+                        newRecords.push(_.cloneDeep(record) )
+                    })
+
+                    deployments.insertMany(newRecords, function(err,insertResults) {
+                        if (err){
+                            const msg = {"error": err}
+                            console.log(msg)
+                            res.status(statusCode.INTERNAL_SERVER_ERROR).json(msg)
+                            throw err
+                        }
+
+                        const msg = {"message": "Success"}
+                        res.status(statusCode.OK).json(msg);
+                    });
+
+                });
+            }
+        })
+    });
 }
- 
