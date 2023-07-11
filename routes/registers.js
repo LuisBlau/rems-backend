18032d8e9ae548b88e1e87ec1f0b140b34a65daf
Caleb Hobson <caleb.hobson@toshibagcs.com>
2023-07-11 16:09:22 -0500

    One day we'll catch 'em all first try.

--- routes/registers.js
+++ routes/registers.js
@@ -475,7 +475,7 @@ module.exports = function (app, connection, log) {
         "filename": j.values.File
       }
     };
-    const sender = sbClient.createSender(req.query["retailerId"].toLowerCase());
+    const sender = sbClient.createSender(j.Retailer.toLowerCase());
     res.send(sender.sendMessages(msgSent));
   });
 
