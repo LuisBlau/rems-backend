0130df77ef4ee4a390aa8df50c0b093d832fcfcd
Caleb Hobson <caleb.hobson@toshibagcs.com>
2023-08-30 08:41:33 -0500

    Updates

--- routes/dumps.js
+++ routes/dumps.js
@@ -1,5 +1,8 @@
 const mongodb = require("mongodb")
 const statusCode = require('http-status-codes').StatusCodes
+const btoa = require("btoa")
+var azureClient = new mongodb.MongoClient("mongodb://pas-test-nosql-db:1Xur1znUvMn4Ny2xW4BwMjN1eHXYPpCniT8eU3nfnnGVtbV7RVUDotMz9E7Un226yrCyjXyukDDSSxLjNUUyaQ%3D%3D@pas-test-nosql-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@pas-test-nosql-db@");
+azureClient.connect();
 
 var azureClient = new mongodb.MongoClient("mongodb://pas-test-nosql-db:1Xur1znUvMn4Ny2xW4BwMjN1eHXYPpCniT8eU3nfnnGVtbV7RVUDotMz9E7Un226yrCyjXyukDDSSxLjNUUyaQ%3D%3D@pas-test-nosql-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@pas-test-nosql-db@");
 azureClient.connect();
