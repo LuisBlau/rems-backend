2eff6bbae8280d38117c2177b8fd45b7746ee1ab
Caleb Hobson <caleb.hobson@toshibagcs.com>
2023-10-10 12:09:17 -0500

    Pull out devices to their own route, add peripherals and filter devices

---
+++ routes/devices.js
@@ -0,0 +1,47 @@
+const mongodb = require("mongodb")
+const statusCode = require('http-status-codes').StatusCodes
+
+var azureClient = new mongodb.MongoClient("mongodb://pas-test-nosql-db:1Xur1znUvMn4Ny2xW4BwMjN1eHXYPpCniT8eU3nfnnGVtbV7RVUDotMz9E7Un226yrCyjXyukDDSSxLjNUUyaQ%3D%3D@pas-test-nosql-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@pas-test-nosql-db@");
+azureClient.connect();
+
+module.exports = function (app) {
+    app.get('/devices/getDevices', (req, res) => {
+        let query = { retailer_id: req.query.retailerId, ip: { $ne: null } }
+        if (req.query["tenantId"] !== undefined) {
+            query.tenant_id = req.query["tenantId"]
+        }
+
+        var devices = azureClient.db("pas_software_distribution").collection("devices");
+        devices.find(query).toArray(function (err, results) {
+            if (err) {
+                const msg = { "error": err }
+                res.status(statusCode.INTERNAL_SERVER_ERROR).json(msg)
+            } else if (results.length === 0) {
+                const msg = { "message": "Rems: Error reading from server" }
+                res.status(statusCode.NO_CONTENT).json(msg);
+            } else {
+                res.status(statusCode.OK).json(results);
+            }
+        })
+    });
+
+    app.get('/devices/getPeripherals', (req, res) => {
+        let query = { retailer_id: req.query.retailerId, ip: { $exists: false } }
+        if (req.query["tenantId"] !== undefined) {
+            query.tenant_id = req.query["tenantId"]
+        }
+
+        var devices = azureClient.db("pas_software_distribution").collection("devices");
+        devices.find(query).toArray(function (err, results) {
+            if (err) {
+                const msg = { "error": err }
+                res.status(statusCode.INTERNAL_SERVER_ERROR).json(msg)
+            } else if (results.length === 0) {
+                const msg = { "message": "Rems: Error reading from server" }
+                res.status(statusCode.NO_CONTENT).json(msg);
+            } else {
+                res.status(statusCode.OK).json(results);
+            }
+        })
+    });
+}
